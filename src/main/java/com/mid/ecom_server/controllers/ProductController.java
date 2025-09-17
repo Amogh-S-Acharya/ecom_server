22a5683299d08f97e9adc26661d83492eccbf91e
Amogh S Acharya <2023ci_amoghsacharya_a@nie.ac.in>
2025-09-17 14:49:05 +0530

    Implement ResourceNotFoundException and enhance error handling in ProductController

--- src/main/java/com/mid/ecom_server/controllers/ProductController.java
+++ src/main/java/com/mid/ecom_server/controllers/ProductController.java
@@ -15,8 +15,11 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.mid.ecom_server.exceptions.ResourceNotFoundException;
 import com.mid.ecom_server.models.Product;
 import com.mid.ecom_server.repos.ProductRepo;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
 
 @RestController
 @RequestMapping("/products")
@@ -24,6 +27,7 @@ public class ProductController {
 	private static final Logger Log = LoggerFactory.getLogger(ProductController.class);
 	@Autowired ProductRepo productRepo;
 	
+	@Tag(name = "Get All Products")
 	@GetMapping("/all")
 	public List<Product> getAllProducts() {
 		Log.info("fetching products");
@@ -39,8 +43,8 @@ public class ProductController {
 	public String deleteProduct(@PathVariable String id) {
 		Optional <Product> findproduct = productRepo.findById(id);
 		if(findproduct.isEmpty()) {
-			productRepo.deleteById(id);
-			return "Failed to delete the Product ";
+			Log.error("Failed to delete product "+id);
+			throw new ResourceNotFoundException("Product Not Found");
 		}
 		productRepo.deleteById(id);
 		Log.info("Product Deleted "+id);
--- src/main/java/com/mid/ecom_server/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/mid/ecom_server/exceptions/GlobalExceptionHandler.java
@@ -1,12 +1,8 @@
 package com.mid.ecom_server.exceptions;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ExceptionHandler;
+
 
 public class GlobalExceptionHandler {
-	@ExceptionHandler(ResourceNotFoundException.class)
-	public ResponseEntity<String> handleNotFound(String msg){
-		ResponseEntity.status(HttpStatus.NOT_FOUND).body(msg);
-	}
+
+
 }
